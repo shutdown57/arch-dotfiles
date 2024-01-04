f2bb8ec127b5bfb1243a22d546321b45c72dd439
Majid Mohamadi <m.shutdown57@gmail.com>
2024-01-04 23:50:32 +0330

    Updated configs and packages

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,18 +2,22 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
-      -- setup = {
-      --   tsserver = function(_, opts)
-      --     opts.capabilities.offsetEncoding = { "utf-16" }
-      --   end,
-      --   biome = function(_, opts)
-      --     opts.capabilities.offsetEncoding = { "utf-16" }
-      --   end,
-      -- },
+      setup = {
+        clangd = function(_, opts)
+          opts.capabilities.offsetEncoding = { "utf-16" }
+        end,
+        tsserver = function(_, opts)
+          opts.capabilities.offsetEncoding = { "utf-16" }
+        end,
+        biome = function(_, opts)
+          opts.capabilities.offsetEncoding = { "utf-16" }
+        end,
+      },
       config = function()
         local capabilities = vim.lsp.protocol.make_client_capabilities()
         capabilities.offsetEncoding = { "utf-16" }
         require("lspconfig").clangd.setup({ capabilities = capabilities })
+        require("lspconfig").biome.setup({ capabilities = capabilities })
         require("lspconfig").tsserver.setup({ capabilities = capabilities })
       end,
     },
