7cdc1d6d4400ea1c285f537d4c16aa80205b815c
Majid Mohamadi Parvar <m.shutdown57@gmail.com>
2024-04-05 16:46:03 +0330

    removed neorg

--- .config/nvim/lua/plugins/rest.lua
+++ .config/nvim/lua/plugins/rest.lua
@@ -8,17 +8,19 @@
 --   end,
 -- }
 
-return {
-  "vhyrro/luarocks.nvim",
-  config = function()
-    require("luarocks").setup({})
-  end,
-},
-{
-  "rest-nvim/rest.nvim",
-  ft = "http",
-  dependencies = { "luarocks.nvim" },
-  config = function()
-    require("rest-nvim").setup()
-  end,
+return{
+  -- {
+  --   "vhyrro/luarocks.nvim",
+  --   config = function()
+  --     require("luarocks").setup({})
+  --   end,
+  -- },
+  {
+    "rest-nvim/rest.nvim",
+    ft = "http",
+    dependencies = { "luarocks.nvim" },
+    config = function()
+      require("rest-nvim").setup()
+    end,
+  }
 }
